e5989f7771f7d0c224874b0fe1246efe5937f76f
Dosakaya <110212899+Dosakaya@users.noreply.github.com>
2023-05-04 17:33:38 +0530

    Update test_Bundleprocessor_ut.py

--- unit_tests/L1_testing/test_files/test_Bundleprocessor_ut.py
+++ unit_tests/L1_testing/test_files/test_Bundleprocessor_ut.py
@@ -24,7 +24,7 @@ print("printing cwd ")
 print(os.getcwd())
 
 from bundlegen.core.bundle_processor import BundleProcessor
-#print(sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+print(sys.path.insert(1, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from unit_tests.L1_testing import get_L1_test_results
 from bundlegen.core.library_matching import LibraryMatching
 from loguru import logger
